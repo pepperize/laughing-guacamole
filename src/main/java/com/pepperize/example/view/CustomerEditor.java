3fc89bccb05e2719eb882cf693f0590adc1e4295
Patrick Florek <patrick.florek@tui.com>
2020-06-07 21:32:41 +0200

    Move customer to main for show case

--- src/main/java/com/pepperize/example/view/CustomerEditor.java
+++ src/main/java/com/pepperize/example/view/CustomerEditor.java
@@ -5,6 +5,7 @@ import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.KeyNotifier;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
@@ -78,6 +79,7 @@ public class CustomerEditor extends VerticalLayout implements KeyNotifier {
     void save() {
         repository.save(customer);
         changeHandler.onChange();
+        Notification.show(String.format("\"%s\" saved.", customer.getLastName()));
     }
 
     public interface ChangeHandler {
--- src/main/java/com/pepperize/example/view/MainView.java
+++ src/main/java/com/pepperize/example/view/MainView.java
@@ -1,16 +1,18 @@
 package com.pepperize.example.view;
 
-import com.pepperize.example.service.GreetService;
-import com.vaadin.flow.component.Key;
+import com.pepperize.example.model.Customer;
+import com.pepperize.example.repository.CustomerRepository;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 
 /**
  * A sample Vaadin view class.
@@ -33,34 +35,61 @@ import org.springframework.beans.factory.annotation.Autowired;
 @CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
 public class MainView extends VerticalLayout {
 
-    /**
-     * Construct a new Vaadin view.
-     * <p>
-     * Build the initial UI state for the user accessing the application.
-     *
-     * @param service The message service. Automatically injected Spring managed bean.
-     */
-    public MainView(@Autowired GreetService service) {
+    private final CustomerRepository repo;
 
-        // Use TextField for standard text input
-        TextField textField = new TextField("Your name");
+    private final Grid<Customer> grid;
 
-        // Button click listeners can be defined as lambda expressions
-        Button button = new Button("Say hello",
-                e -> Notification.show(service.greet(textField.getValue())));
+    public MainView(CustomerRepository repo, CustomerEditor editor) {
+        this.repo = repo;
 
-        // Theme variants give you predefined extra styles for components.
-        // Example: Primary button has a more prominent look.
-        button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        this.grid = new Grid<>(Customer.class);
 
-        // You can specify keyboard shortcuts for buttons.
-        // Example: Pressing enter in this view clicks the Button.
-        button.addClickShortcut(Key.ENTER);
+        final TextField filter = new TextField();
 
-        // Use custom CSS classes to apply styling. This is defined in shared-styles.css.
-        addClassName("centered-content");
+        final Button addNewBtn = new Button("New customer", VaadinIcon.PLUS.create());
 
-        add(textField, button);
+        // build layout
+        HorizontalLayout actions = new HorizontalLayout(filter, addNewBtn);
+        add(actions, grid, editor);
+
+        grid.setHeight("300px");
+        grid.setColumns("id", "firstName", "lastName");
+        grid.getColumnByKey("id").setWidth("50px").setFlexGrow(0);
+
+        filter.setPlaceholder("Filter by last name");
+
+        // Hook logic to components
+
+        // Replace listing with filtered content when user changes filter
+        filter.setValueChangeMode(ValueChangeMode.EAGER);
+        filter.addValueChangeListener(e -> listCustomers(e.getValue()));
+
+        // Connect selected Customer to editor or hide if none is selected
+        grid.asSingleSelect().addValueChangeListener(e -> {
+            editor.editCustomer(e.getValue());
+        });
+
+        // Instantiate and edit new Customer the new button is clicked
+        addNewBtn.addClickListener(e -> editor.editCustomer(new Customer("", "")));
+
+        // Listen changes made by the editor, refresh data from backend
+        editor.setChangeHandler(() -> {
+            editor.setVisible(false);
+            listCustomers(filter.getValue());
+        });
+
+        // Initialize listing
+        listCustomers(null);
     }
+
+    // tag::listCustomers[]
+    void listCustomers(String filterText) {
+        if (StringUtils.isEmpty(filterText)) {
+            grid.setItems(repo.findAll());
+        } else {
+            grid.setItems(repo.findByLastNameStartsWithIgnoreCase(filterText));
+        }
+    }
+    // end::listCustomers[]
 
 }
